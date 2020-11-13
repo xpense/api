d4cbb0ba88a4011a90a5e2685cf042b35a294d4e
sanevillain <denis.ismail@code.berlin>
2020-11-13 16:12:44 +0100

    :recycle: Refactor NewListResponse func

--- handlers/handlers_payload.go
+++ handlers/handlers_payload.go
@@ -4,21 +4,19 @@ import (
 	"reflect"
 )
 
+// ListResponse is a 'generic' json response whenever the response is a list of something
 type ListResponse struct {
 	Count   int           `json:"count"`
 	Entries []interface{} `json:"entries"`
 }
 
+// NewListResponse creates a list response out of a slice of interfaces
 func NewListResponse(slice interface{}) *ListResponse {
-	s := reflect.ValueOf(slice)
-
-	var entries []interface{}
-	if s.Kind() == reflect.Slice {
-
-		entries = make([]interface{}, s.Len())
-
+	entries := []interface{}{}
+	if s := reflect.ValueOf(slice); s.Kind() == reflect.Slice {
 		for i := 0; i < s.Len(); i++ {
-			entries[i] = s.Index(i).Interface()
+			entry := s.Index(i).Interface()
+			entries = append(entries, entry)
 		}
 	}
 
