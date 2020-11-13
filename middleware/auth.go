3f7877f444f9991a2b3246b7d6f45acf7bfda262
sanevillain <denis.ismail@code.berlin>
2020-11-13 17:45:07 +0100

    :recycle: Refactor auth middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -8,39 +8,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const claimsKey = "claims"
-
-func Auth() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		authHeader := strings.Split(ctx.GetHeader("Authorization"), "Bearer ")
-		if len(authHeader) != 2 {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"message": "Malformed token",
-			})
-			return
-		}
-
-		claims, err := utils.ValidateJWT(authHeader[1])
-		if err != nil {
-			ctx.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-
-		ctx.Set(claimsKey, claims)
-		ctx.Next()
-	}
+type AuthMiddleware interface {
+	Handler(*gin.Context)
 }
 
-func GetClaimsFromContext(ctx *gin.Context) *utils.CustomClaims {
-	val, exists := ctx.Get(claimsKey)
-	if !exists {
-		return nil
+type authMiddleware struct {
+	jwtService utils.JWTService
+}
+
+func NewAuthMiddleware(jwtService utils.JWTService) AuthMiddleware {
+	return &authMiddleware{jwtService}
+}
+
+func (a *authMiddleware) Handler(ctx *gin.Context) {
+	authHeader := strings.Split(ctx.GetHeader("Authorization"), "Bearer ")
+	if len(authHeader) != 2 {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "Malformed token",
+		})
+		return
 	}
 
-	claims, ok := val.(*utils.CustomClaims)
-	if !ok {
-		return nil
+	claims, err := a.jwtService.ValidateJWT(authHeader[1])
+	if err != nil {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
-	return claims
+	utils.SetClaimsToContext(ctx, claims)
+	ctx.Next()
 }
