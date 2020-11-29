2735a091ce42ecb99a52c3e319357cdfa2853971
sanevillain <denis.ismail@code.berlin>
2020-11-29 21:38:26 +0100

    :white_check_mark: Add integration testing setup

---
+++ test/integration/setup.go
@@ -0,0 +1,52 @@
+package integration
+
+import (
+	"expense-api/internal/app"
+	"expense-api/internal/middleware/auth"
+	"expense-api/internal/repository"
+	"expense-api/internal/router"
+	"expense-api/internal/utils"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	TEST_JWT_ISSUER  = "TEST_JWT_ISSUER"
+	TEST_JWT_SECRET  = "TEST_JWT_SECRET"
+	TEST_DB_USER     = "TEST_DB_USER"
+	TEST_DB_PASSWORD = "TEST_DB_PASSWORD"
+	TEST_DB_HOST     = "TEST_DB_HOST"
+	TEST_DB_NAME     = "TEST_DB_NAME"
+)
+
+func NewTestingEnvironment() *app.Environment {
+	return &app.Environment{
+		Issuer:     app.EnvironmentVariable{Name: TEST_JWT_ISSUER},
+		Secret:     app.EnvironmentVariable{Name: TEST_JWT_SECRET},
+		DBUser:     app.EnvironmentVariable{Name: TEST_DB_USER},
+		DBPassword: app.EnvironmentVariable{Name: TEST_DB_PASSWORD},
+		DBHost:     app.EnvironmentVariable{Name: TEST_DB_HOST},
+		DBName:     app.EnvironmentVariable{Name: TEST_DB_NAME},
+	}
+}
+
+func Setup() *gin.Engine {
+	env := NewTestingEnvironment()
+	env.LoadVariables()
+
+	dbConn, err := repository.NewConnection(env.DBUser.Value, env.DBPassword.Value, env.DBHost.Value, env.DBName.Value)
+	if err != nil {
+		panic(fmt.Sprintf("couldn't establish postgres connection: %v", err))
+	}
+
+	if err := repository.Migrate(dbConn); err != nil {
+		panic(fmt.Sprintf("error setting up database: %v", err))
+	}
+
+	repository := repository.New(dbConn)
+	jwtService := auth.NewJWTService(env.Issuer.Value, env.Secret.Value)
+	hasher := utils.NewPasswordHasher()
+
+	return router.Setup(repository, jwtService, hasher, router.DefaultConfig)
+}
